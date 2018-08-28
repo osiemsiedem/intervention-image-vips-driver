cc89511dc851626860c15ee3a090d425cb452ed9
Marek Szymczuk <marek@osiemsiedem.com>
2018-08-28 20:02:44 +0200

    Working on Fill command

--- src/Commands/FillCommand.php
+++ src/Commands/FillCommand.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Vips\Commands;
 
-use Intervention\Image\Exception\NotSupportedException;
+use Jcupitt\Vips\Extend;
+use Intervention\Image\Image;
+use Intervention\Image\Vips\Color;
+use Intervention\Image\Exception\NotReadableException;
 
 class FillCommand extends AbstractCommand
 {
@@ -12,11 +15,55 @@ class FillCommand extends AbstractCommand
      * Execute the command.
      *
      * @param  \Intervention\Image\Image  $image
-     * @return void
-     * @throws \Intervention\Image\Exception\NotSupportedException
+     * @return bool
      */
-    public function execute($image): void
+    public function execute($image): bool
     {
-        throw new NotSupportedException('Fill command is not supported by VIPS driver.');
+        $filling = $this->argument(0)->value();
+
+        $x = $this->argument(1)
+            ->type('digit')
+            ->value();
+
+        $y = $this->argument(2)
+            ->type('digit')
+            ->value();
+
+        return $this->handleCommand(function () use ($image, $filling, $x, $y) {
+            $core = $image->getCore();
+
+            try {
+                $filling = $image->getDriver()->decoder->init($filling);
+            } catch (NotReadableException $e) {
+                $filling = new Color($filling);
+            }
+
+            if (is_int($x) && is_int($y)) {
+                if ($filling instanceof Image) {
+                    //
+                } elseif ($filling instanceof Color) {
+                    //
+                }
+            } else {
+                if ($filling instanceof Image) {
+                    $overlayCore = $filling->getCore();
+
+                    $overlayCore = $overlayCore->embed(0, 0, $core->width, $core->height, [
+                        'extend'     => Extend::REPEAT,
+                        'background' => [0, 0, 0, 0],
+                    ]);
+
+                    $core = $core->composite([$core, $overlayCore], 2);
+                } elseif ($filling instanceof Color) {
+                    $overlay = $image->getDriver()->newImage($core->width, $core->height, $filling->getArray());
+
+                    $overlayCore = $overlay->getCore();
+
+                    $core = $core->composite([$core, $overlayCore], 2);
+                }
+            }
+
+            $image->setCore($core);
+        });
     }
 }
