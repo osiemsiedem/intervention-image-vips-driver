e819e0ca759fd1363d75d4c9240e195d35e12301
Jakub Caban <kuba@whyblack.pl>
2020-09-24 14:40:11 +0200

    Properly use font file and make positioning consistent with GD

--- src/Font.php
+++ src/Font.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Vips;
 
+use FontLib\Exception\FontNotFoundException;
+use FontLib\Font as FontReader;
 use Intervention\Image\AbstractFont;
 use Intervention\Image\Exception\RuntimeException;
 use Intervention\Image\Image;
@@ -40,30 +42,90 @@ class Font extends AbstractFont
             ]
         );
 
+        $angle = deg2rad($this->angle);
+        $posy += $core->height * cos($angle);
+
+        $angle = -1 * ($this->angle % 365);
+        switch ($angle) {
+            case 0:
+                break;
+
+            case 90:
+            case -270:
+                $core = $core->rot90();
+                break;
+
+            case 180:
+            case -180:
+                $core = $core->rot180();
+                break;
+
+            case -90:
+            case 270:
+                $core = $core->rot270();
+                break;
+
+            default:
+                $core = $core->rotate($angle, [
+                    'background' => [0],
+                ]);
+        }
+
         $align = is_null($this->align) ? 'left' : strtolower($this->align);
         $valign = is_null($this->valign) ? 'bottom' : strtolower($this->valign);
 
-        // corrections on y-position
-        switch ($valign) {
-            case 'center':
-            case 'middle':
-                $posx -= round($core->width / 2);
+        $box = $this->getBoxSize();
+
+        $posy -= $core->height;
+        // correction on position depending on v/h alignment
+        switch ($align.'-'.$valign) {
+
+            case 'center-top':
+                $posx -= round(($box[6] + $box[4])/2);
+                $posy -= round(($box[7] + $box[5])/2);
                 break;
 
-            case 'right':
-                $posx -= $core->width;
-                break;
-        }
-
-        // corrections on y-position
-        switch ($align) {
-            case 'center':
-            case 'middle':
-                $posy -= round($core->height / 2);
+            case 'right-top':
+                $posx -= $box[4];
+                $posy -= $box[5];
                 break;
 
-            case 'bottom':
-                $posy -= $core->height;
+            case 'left-top':
+                $posx -= $box[6];
+                $posy -= $box[7];
+                break;
+
+            case 'center-center':
+            case 'center-middle':
+                $posx -= round(($box[0] + $box[4])/2);
+                $posy -= round(($box[1] + $box[5])/2);
+                break;
+
+            case 'right-center':
+            case 'right-middle':
+                $posx -= round(($box[2] + $box[4])/2);
+                $posy -= round(($box[3] + $box[5])/2);
+                break;
+
+            case 'left-center':
+            case 'left-middle':
+                $posx -= round(($box[0] + $box[6])/2);
+                $posy -= round(($box[1] + $box[7])/2);
+                break;
+
+            case 'center-bottom':
+                $posx -= round(($box[0] + $box[2])/2);
+                $posy -= round(($box[1] + $box[3])/2);
+                break;
+
+            case 'right-bottom':
+                $posx -= $box[2];
+                $posy -= $box[3];
+                break;
+
+            case 'left-bottom':
+                $posx -= $box[0];
+                $posy -= $box[1];
                 break;
         }
 
@@ -104,8 +166,31 @@ class Font extends AbstractFont
             $box['height'] = 0;
         } else {
             $core = $this->createTextImage();
-            $box['width'] = $core->width;
-            $box['height'] = $core->height;
+            $box = [
+                $core->xoffset,
+                $core->height,
+                $core->width+$core->xoffset,
+                $core->height,
+                $core->width+$core->xoffset,
+                0,
+                $core->xoffset,
+                0
+            ];
+
+            if ($this->angle !== 0) {
+
+                $angle = deg2rad(360 - $this->angle);
+
+                for ($i=0; $i<4; $i++) {
+                    $x = $box[$i * 2];
+                    $y = $box[$i * 2 + 1];
+                    $box[$i * 2] = cos($angle) * $x - sin($angle) * $y;
+                    $box[$i * 2 + 1] = sin($angle) * $x + cos($angle) * $y;
+                }
+            }
+
+            $box['width'] = (int) abs($box[4] - $box[0]);
+            $box['height'] = (int) abs($box[5] - $box[1]);
         }
 
         return $box;
@@ -113,18 +198,18 @@ class Font extends AbstractFont
 
     /**
      * @return ImageCore
-     * @throws Exception
+     * @throws Exception|FontNotFoundException
      */
     private function createTextImage()
     {
         if ($this->hasApplicableFontFile()) {
-            $info = pathinfo($this->file);
-            $font = basename($this->file, '.'.$info['extension']);
+            $font = FontReader::load($this->file);
         } else {
             throw new RuntimeException(
                 'Font file must be provided to apply text to image.'
             );
         }
+
         $align_to_vips = [
             'left' => Align::LOW,
             'right' => Align::CENTRE,
@@ -133,37 +218,11 @@ class Font extends AbstractFont
         $core = ImageCore::text(
             $this->text,
             [
-                'font' => $font.' '.$this->size,
+                'font' => $font->getFontFullName().' '.$this->size,
                 'fontfile' => $this->file,
                 'align' => $align_to_vips[$this->align ?: 'left'],
             ]
         );
-
-        switch ($this->angle % 360) {
-            case 0:
-                break;
-
-            case 90:
-            case -270:
-                $core = $core->rot90();
-                break;
-
-            case 180:
-            case -180:
-                $core = $core->rot180();
-                break;
-
-            case -90:
-            case 270:
-                $core = $core->rot270();
-                break;
-
-            default:
-                $core = $core->similarity([
-                    'background' => [0],
-                    'angle' => $this->angle % 360,
-                ]);
-        }
 
         return $core;
     }
